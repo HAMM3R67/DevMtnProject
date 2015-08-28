8c079a07bb68b171b7c01d02d936e862589c7cba
HAMM3R67 <tcschauer@gmail.com>
2015-08-27 20:25:04 -0600

    Set up some routes, still need to connect them

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-var app = angular.module('recipeApp', []);
+var app = angular.module('recipeApp', ['ngRoute']);
+// .constant('fb', {
+//     url: 'https://hammers-rtfm-app.firebaseio.com/'
+//     })
+app.config(function($routeProvider){
+        $routeProvider.
+            when('/', {
+                templateUrl: 'index.html'
+            }).
+            when('/recipes', {
+                templateUrl: 'recipes/recipe.html'
+            }).
+            otherwise({
+                redirectTo: '/'
+            })
+});
--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -2,12 +2,14 @@ var app = angular.module('recipeApp');
 
 app.controller('recipeController', function($scope, recipeService){
 	
-	$scope.getRecipeData = function(){
-		recipeService.getRecipe($scope.searchRecipe).then(function(result){
-			console.log(result)
-			$scope.recipe = result.title
-			$scope.searchRecipe = ''
-		})
-	}
+	
+	
+	// $scope.getRecipeData = function(){
+	// 	recipeService.getRecipe($scope.searchRecipe).then(function(result){
+	// 		console.log(result)
+	// 		$scope.recipe = result.title
+	// 		$scope.searchRecipe = ''
+	// 	})
+	// }
 	
 });
