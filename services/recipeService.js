e4a71a7d67dc8f8a96d5c4ce9614667d7546b00e
HAMM3R67 <tcschauer@gmail.com>
2015-08-28 21:50:05 -0600

    created a defaultHeaders.js

--- services/recipeService.js
+++ services/recipeService.js
@@ -1,7 +1,9 @@
 var app = angular.module('refrigiChef');
 
-app.service('recipeService', function($http){
+app.service('recipeService', function($http, $q){
+	
 	var key = 'd4c39b34edfa725f0882fc2008161d90'
+	
 	
 	
 })
