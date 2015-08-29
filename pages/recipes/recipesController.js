d171c04bcec2c99f9befcb43a477eec033bdc795
HAMM3R67 <tcschauer@gmail.com>
2015-08-29 11:16:36 -0600

    trying to perform a get request with food2fork

--- pages/recipes/recipesController.js
+++ pages/recipes/recipesController.js
@@ -3,7 +3,7 @@ var app = angular.module('refrigiChef');
 app.controller('recipesController', function($scope, recipeService){
 	
 	$scope.getRecipe = function(){
-		recipeService.getRecipes($scope.ingredients).then(results){
+		recipeService.getRecipes($scope.ingredients).then(function(results)){
 			$scope.recipes = results
 		}
 	}
