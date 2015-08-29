33679a71dc9395b0947a05fd76740bf83bae5b12
HAMM3R67 <tcschauer@gmail.com>
2015-08-29 11:47:47 -0600

    I GOT THE API DATA WORKING!!!

--- pages/recipes/recipesController.js
+++ pages/recipes/recipesController.js
@@ -3,9 +3,10 @@ var app = angular.module('refrigiChef');
 app.controller('recipesController', function($scope, recipeService){
 	
 	$scope.getRecipe = function(){
-		recipeService.getRecipes($scope.ingredients).then(function(results)){
+		recipeService.getRecipes($scope.ingredients).then(function(results){
+			console.log(results.data)
 			$scope.recipes = results
-		}
+		})
 	}
 	
 })
--- services/recipeService.js
+++ services/recipeService.js
@@ -5,7 +5,7 @@ app.service('recipeService', function($http, $q){
 	var key = 'd4c39b34edfa725f0882fc2008161d90'
 	
 	this.getRecipes = function(ingredient){
-		return $http.get('http://food2fork.com/api/get' + ingredient)
+		return $http.get('http://food2fork.com/api/search?key=' + key + '&q=' + ingredient)
 	}
 	
 	
