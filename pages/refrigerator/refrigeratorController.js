d800fe213e6d5a04d9b8fa51afb17d095d93be6f
HAMM3R67 <tcschauer@gmail.com>
2015-08-31 05:45:46 -0600

    added ng repeat with recipe title and img

--- pages/refrigerator/refrigeratorController.js
+++ pages/refrigerator/refrigeratorController.js
@@ -4,9 +4,9 @@ app.controller('refrigeratorController', function($scope, recipeService){
 	
 	$scope.getRecipe = function(){
 		//gets a list of recipes based off the ingredients that are input into the box
-		recipeService.getRecipes($scope.ingredients).then(function(results){
-			console.log(results.data)
-			$scope.recipes = results
+		recipeService.getRecipes($scope.ingredient1, $scope.ingredient2, $scope.ingredient3, $scope.ingredient4, $scope.ingredient5).then(function(results){
+			console.log(results.data.recipes)
+			$scope.recipes = results.data.recipes
 		})
 	}
 })
