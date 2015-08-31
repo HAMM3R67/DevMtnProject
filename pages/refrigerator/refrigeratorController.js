0ac293280f01138a57944155fc8d3b9ac74610ae
HAMM3R67 <tcschauer@gmail.com>
2015-08-31 05:06:54 -0600

    added the ingredients search to the refrigerator page

--- pages/refrigerator/refrigeratorController.js
+++ pages/refrigerator/refrigeratorController.js
@@ -2,4 +2,11 @@ var app = angular.module('refrigiChef');
 
 app.controller('refrigeratorController', function($scope){
 	
+	$scope.getRecipe = function(){
+		//gets a list of recipes based off the ingredients that are input into the box
+		recipeService.getRecipes($scope.ingredients).then(function(results){
+			console.log(results.data)
+			$scope.recipes = results
+		})
+	}
 })
