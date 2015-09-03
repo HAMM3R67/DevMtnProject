97210a0900f5e732216285c13c1c131f3cfec1ba
HAMM3R67 <tcschauer@gmail.com>
2015-09-02 19:56:29 -0600

    working on posting recipes to firebase

--- pages/refrigerator/refrigeratorController.js
+++ pages/refrigerator/refrigeratorController.js
@@ -6,8 +6,9 @@ app.controller('refrigeratorController', function($scope, recipeService, userSer
 	
 	$scope.getRecipe = function(){
 		//gets a list of recipes based off the ingredients that are input into the box
-		recipeService.getRecipes($scope.ingredient1, $scope.ingredient2, $scope.ingredient3, $scope.ingredient4, $scope.ingredient5).then(function(results){
-			console.log(results)
+		recipeService.getRecipes($scope.ingredient1, $scope.ingredient2, $scope.ingredient3, $scope.ingredient4, $scope.ingredient5)
+		.then(function(results){
+			console.log(results.data.recipes)
 			$scope.recipes = results.data.recipes
 			//The below will reset the input boxes once the button has been clicked and data returned
 			$scope.ingredient1 = ""; 
@@ -22,7 +23,10 @@ app.controller('refrigeratorController', function($scope, recipeService, userSer
 	var currentUser = userService.getLoggedInUser();
 	
 	$scope.saveRecipe = function(){
-		
+		recipeService.saveRecipes($scope.selectedRecipe).then(function(savedResults){
+			console.log(savedResults.data)
+			$scope.savedRecipes = savedResults.data.recipes
+		})
 	}
 	
 })
