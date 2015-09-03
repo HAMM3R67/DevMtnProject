1c75bb7338578e143defb37dacc3cf55a9b1a0b0
HAMM3R67 <tcschauer@gmail.com>
2015-09-02 18:58:29 -0600

    working on email login

--- pages/home/homeController.js
+++ pages/home/homeController.js
@@ -10,4 +10,21 @@ app.controller('homeController', function($scope, $location, userService){
 		userService.loginWithFacebook();
 	}
 	
+	//below is email verification code, I am not sure exactly how it works...
+// 	$scope.authObj.$createUser({
+// 		email: "my@email.com",
+// 		password: "mypassword"
+// 	}).then(function(userData) {
+//  		 console.log("User " + userData.uid + " created successfully!");
+// 
+//   	return $scope.authObj.$authWithPassword({
+// 		email: "my@email.com",
+// 		password: "mypassword"
+//   	});
+// 	}).then(function(authData) {
+// 		console.log("Logged in as:", authData.uid);
+// 	}).catch(function(error) {
+// 		console.error("Error: ", error);
+// 	});
+	
 })
