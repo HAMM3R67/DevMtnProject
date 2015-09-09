84a2c75cf8c137c46d9f3d8a981035335521f776
HAMM3R67 <tcschauer@gmail.com>
2015-09-09 00:02:15 -0600

    time to get the recipes to SAVE

--- pages/refrigerator/refrigeratorController.js
+++ pages/refrigerator/refrigeratorController.js
@@ -6,14 +6,12 @@ app.controller('refrigeratorController', function($scope, $location, $routeParam
 	
 	$scope.getRecipe = function(){
 		//gets a list of recipes based off the ingredients that are input into the box
-		recipeService.getRecipes($scope.ingredient1, $scope.ingredient2, $scope.ingredient3)
+		recipeService.getRecipes($scope.ingredient)
 		.then(function(results){
 			console.log(results.data.recipes)
 			$scope.recipes = results.data.recipes
 			//The below will reset the input boxes once the button has been clicked and data returned
-			$scope.ingredient1 = ""; 
-			$scope.ingredient2 = ""; 
-			$scope.ingredient3 = "";
+			$scope.ingredient = ""; 			
 		})
 	}
 	
@@ -24,55 +22,12 @@ app.controller('refrigeratorController', function($scope, $location, $routeParam
 		userService.logout();
 	}
 	//save Recipes to firebase under userID
-	var ref = new Firebase('https://refrigi-chef.firebaseio.com/recipes');
-	
-	var recipes = $firebaseArray(ref);
-	
-	$scope.saveRecipe = function (data){
-		recipes.$add(data).then(function(){
-			console.log('Recipe successfully saved to firebase!!!')
-		})
-	}
-})
-	
-	// $scope.saveRecipe = function(){
-	// 	//$scope.selectedRecipe isn't what needs to be passed in getRecipes...the recipe id
-	// 	//needs to pass in that function
-	// 	recipeService.getRecipes($scope.selectedRecipe).then(function(savedResults){
-	// 		console.log(savedResults.data.recipes.recipe_id)
-	// 		$scope.savedRecipes = savedResults.data.recipes.recipe_id
-	// 	})
-	//   }
-	// })
-	
-	//creating a new Firebase array
-// 	var ref = new Firebase('https://refrigi-chef.firebaseio.com/recipes');
-// ​
-//     var recipes = $firebaseArray(ref);
-// ​
-// 	(function(data){
-// 		recipes.$add(data).then(function(){
-// 			console.log('recipe succesfully saved to firebase!!')
-// 		});
-// 	})
 	
 	
 	
-	//I want to be able to select recipes that are returned from the API and save them to Firebase
-	// var recipeId = parseInt($routeParams.recipeId)
-	// 
-	// $scope.selectedRecipe = {};
-	// var savedRecipes = recipeService.saveRecipes(recipes);
-	// 
-	// savedRecipes.$bindTo($scope, 'savedRecipes')// creates $scope.savedRecipes with a 3 way binding
-	// 
-	// $scope.saveRecipe = function(){
-	// 	if(!$scope.selectedRecipe){
-	// 		return false; //Don't do anything if no recipes are selected
-	// 	}
-	// }
-	// 
-	// var newRecipe = {
-	// 	
-	// }
+	
+	
+	
+	//end of controller
+	});
 	
