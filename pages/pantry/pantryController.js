36b13e9750c1e27abc74e2246c0120a2b4f179b8
HAMM3R67 <tcschauer@gmail.com>
2015-09-08 22:21:59 -0600

    added the refrigichef logo to the folder

--- pages/pantry/pantryController.js
+++ pages/pantry/pantryController.js
@@ -3,9 +3,8 @@ var app = angular.module('refrigiChef');
 app.controller('pantryController', function($scope, userService, recipeService){
 	
 	$scope.addPantryItem = function(){
-		recipeService.addPantryItem($scope.pantryItem).then(function(data){
-			$scope.pantryItems = data
-		})
+		recipeService.addPantryItem($scope.pantryItem)
+			
 	}
 })
 	
