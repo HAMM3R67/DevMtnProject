ead3d60975b8234e99c4f15c92889cd495f81d1a
HAMM3R67 <tcschauer@gmail.com>
2015-09-09 23:32:53 -0600

    Did a binding test

--- pages/refrigerator/refrigeratorController.js
+++ pages/refrigerator/refrigeratorController.js
@@ -32,7 +32,18 @@ app.controller('refrigeratorController', function($scope, $location, $routeParam
 	
 	$scope.savedRecipes.$save()
 	
+	//testing 3 way data binding with Firebase
+		var ref = new Firebase("https://refrigi-chef.firebaseio.com/data");
+		// download the data into a local object
+		$scope.data = $firebaseObject(ref);
+		// putting a console.log here won't work, see below
+		
+		// download the data into a local object
+		var syncObject = $firebaseObject(ref);
 	
+		 // synchronize the object with a three-way data binding
+		// click on `index.html` above to see it used in the DOM!
+		syncObject.$bindTo($scope, "data");
 	
 	
 	
