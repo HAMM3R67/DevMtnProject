55bb155fdc009ae47003c07837696a1e584902da
HAMM3R67 <tcschauer@gmail.com>
2015-09-29 06:39:44 -0600

    removed https...doesn't work

--- services/recipeService.js
+++ services/recipeService.js
@@ -5,12 +5,12 @@ app.service('recipeService', function($http, $q, $firebaseArray, $firebaseObject
 	var key = 'd4c39b34edfa725f0882fc2008161d90'
 	
 	this.getRecipes = function(ingredient){
-		return $http.get('https://food2fork.com/api/search?key=' + key + '&q=' + ingredient)
+		return $http.get('http://food2fork.com/api/search?key=' + key + '&q=' + ingredient)
 		
 	}
 	
 	this.getRecipe = function(rId){
-		return $http.get('https://food2fork.com/api/get?key=' + key + '&' + rId)
+		return $http.get('http://food2fork.com/api/get?key=' + key + '&' + rId)
 			.success(function(data){
 				recipes.$add(data).then(function(){
 					console.log('recipe succesfully saved to firebase!!')
